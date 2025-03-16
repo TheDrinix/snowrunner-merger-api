9b83623d8335a92ab670bc1b019e309c69c93a37
Drinix <thedrinix@gmail.com>
2025-03-16 22:31:40 +0100

    Create migration for adding google id to user

---
+++ Migrations/20250314222209_ExpandUserWithGoogleId.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SnowrunnerMergerApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExpandUserWithGoogleId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GoogleId",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_GoogleId",
+                table: "Users",
+                column: "GoogleId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_NormalizedEmail",
+                table: "Users",
+                column: "NormalizedEmail");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Email",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_GoogleId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_NormalizedEmail",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "GoogleId",
+                table: "Users");
+        }
+    }
+}
